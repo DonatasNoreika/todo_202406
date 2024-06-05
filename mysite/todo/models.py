ad92f43d968f3ea14251f56d3b7c4ab3e39ca947
DonatasNoreika <donoras@gmail.com>
2024-06-05 17:14:06 +0300

    +Aprašyti projekto modulius (į) - pavadinimas, vartotojas, įvykdyta/neįvykdyta

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=20)
+    summary = models.TextField(verbose_name="Summary", max_length=2000)
+    date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
+    user = models.ForeignKey(to=User, verbose_name="User", on_delete=models.CASCADE)
+    is_done = models.BooleanField(verbose_name="Is Done", default=False)
