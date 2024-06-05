9092011758b277609acb2895050f4934d9d511dc
DonatasNoreika <donoras@gmail.com>
2024-06-05 17:47:56 +0300

    +Padaryti bendrą taskų atvaizdavimo puslapį

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Task
 
 # Create your views here.
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "tasks.html"
