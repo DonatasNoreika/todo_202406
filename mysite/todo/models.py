4a5f3a3bc4b6dfa8004966a1571df4ebd48577e6
DonatasNoreika <donoras@gmail.com>
2024-06-05 17:25:35 +0300

    +Pridėti į admin, aprašyti

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(verbose_name="Title", max_length=20)
+    title = models.CharField(verbose_name="Title", max_length=50)
     summary = models.TextField(verbose_name="Summary", max_length=2000)
     date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
     user = models.ForeignKey(to=User, verbose_name="User", on_delete=models.CASCADE)
     is_done = models.BooleanField(verbose_name="Is Done", default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.user})"
